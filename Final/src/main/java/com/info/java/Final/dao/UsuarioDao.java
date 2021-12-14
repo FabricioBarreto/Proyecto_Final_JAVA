c27d1ee6faa65374f65cfb2a03bf2956862330d1
Fabricio pc Negocio <fabrib40@gmail.com>
2021-12-13 21:17:14 -0300

    Primer commit

---
+++ Final/src/main/java/com/info/java/Final/dao/UsuarioDao.java
@@ -0,0 +1,17 @@
+package com.info.java.Final.dao;
+
+import com.info.java.Final.models.Usuario;
+
+import java.util.List;
+
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+
+    //void eliminar(Long id);
+
+    //void registrar(Usuario usuario);
+
+    //Usuario obtenerUsuarioPorCredenciales(Usuario usuario);
+}
