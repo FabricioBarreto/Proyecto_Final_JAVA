9b8ca62d900cbfa38ce8d1f785f4db2dbcb64993
fabrib26 <80056941+fabrib26@users.noreply.github.com>
2021-12-22 17:36:34 -0300

    Delete UrlRpository.java

--- final/src/main/java/com/example/demo/repository/UrlRpository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Url;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UrlRpository extends JpaRepository<Url,Long> {
-}
